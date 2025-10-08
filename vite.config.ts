73e2dc87fe9fecd51f62c84d25b2387b80a5e62c
robertebirdiii-oss <robertebirdiii@gmail.com>
2025-10-07 23:06:44 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: [
+      '1ed970a3-48bb-4a0c-aee8-981587683582-00-c90r89ozytd3.kirk.replit.dev'
+    ]
+  }
 })
